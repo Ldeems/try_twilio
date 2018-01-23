a258e0f1410110d5e94363dbdfa7a1e951d8670a
Ldeems <luke.deems@yahoo.com>
2018-01-23 09:57:11 -0500

    trying the respons, need heroku

--- app.rb
+++ app.rb
@@ -1,6 +1,17 @@
 require 'rubygems'
 require 'twilio-ruby'
 require 'sinatra'
-get "/" do 
-    "Ahoy"
-end
+# get "/" do 
+#     "Ahoy"
+# end
+
+# Respond to incoming calls with a simple text message
+post '/sms' do
+    twiml = Twilio::TwiML::MessagingResponse.new do |r|
+      r.message body: 'The Robots are coming! Head for the hills!'
+    end
+  
+    content_type 'text/xml'
+  
+    twiml.to_s
+  end
