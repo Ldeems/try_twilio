36ce2b1e12dd56e0f0e2c24a8832786c953e32d8
Ldeems <luke.deems@yahoo.com>
2018-01-23 10:23:04 -0500

    to heroku again

--- app.rb
+++ app.rb
@@ -7,6 +7,7 @@ require 'sinatra'
 
 # Respond to incoming calls with a simple text message
 get '/sms' do
+    "so i see somthing"
     twiml = Twilio::TwiML::MessagingResponse.new do |r|
       r.message body: 'The Robots are coming! Head for the hills!'
     end
