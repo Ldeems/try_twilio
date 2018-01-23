6583d8ddf3ede9dfac80cf2254543a9fbd6969ab
Ldeems <luke.deems@yahoo.com>
2018-01-23 12:25:02 -0500

    set up a simple input to send text, gonna try it out now

--- app.rb
+++ app.rb
@@ -2,29 +2,35 @@ require 'rubygems'
 require 'twilio-ruby'
 require 'sinatra'
 load './local_ENV.rb' if File.exist?('./local_ENV.rb')
-get "/" do 
+get '/' do
+    erb :page_one
+end    
+
+post "/send" do 
+    message = params[:message]
     @client = Twilio::REST::Client.new(ENV['account_sid'], ENV['auth_token'])
     
     @message = @client.messages.create(
       from: ENV['twilio_number'],
       to: ENV['to_number'],
-      body: 'they are behind you'
+      body: message
     )
     
     puts @message.status
+    redirect '/'
 end
 
 # Respond to incoming calls with a simple text message
-post '/sms' do
-    "so i see somthing"
-    twiml = Twilio::TwiML::MessagingResponse.new do |r|
-      r.message body: 'The Robots are coming! Head for the hills!'
-    end
+# post '/sms' do
+#     "so i see somthing"
+#     twiml = Twilio::TwiML::MessagingResponse.new do |r|
+#       r.message body: 'The Robots are coming! Head for the hills!'
+#     end
   
-    content_type 'text/xml'
+#     content_type 'text/xml'
   
-    twiml.to_s
-  end
+#     twiml.to_s
+#   end
 
   
 # set up a client to talk to the Twilio REST API
