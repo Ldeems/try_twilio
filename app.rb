023046885885ec0d0b37b7789914034f601096c4
Ldeems <luke.deems@yahoo.com>
2018-01-23 10:28:26 -0500

    once again to heroku

--- app.rb
+++ app.rb
@@ -1,12 +1,12 @@
 require 'rubygems'
 require 'twilio-ruby'
 require 'sinatra'
-# get "/" do 
-#     "Ahoy"
-# end
+get "/" do 
+    "Ahoy"
+end
 
 # Respond to incoming calls with a simple text message
-get '/sms' do
+post '/sms' do
     "so i see somthing"
     twiml = Twilio::TwiML::MessagingResponse.new do |r|
       r.message body: 'The Robots are coming! Head for the hills!'
