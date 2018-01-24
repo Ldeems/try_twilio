5a57ce1524139ab68a6a99e8157e05ae76c9fcaa
Ldeems <luke.deems@yahoo.com>
2018-01-24 10:45:34 -0500

    continueing to try and recieve a message

--- app.rb
+++ app.rb
@@ -19,3 +19,14 @@ post "/send" do
     puts @message.status
     redirect '/'
 end
+
+# Respond to incoming calls with a simple text message
+post '/sms' do
+    twiml = Twilio::TwiML::MessagingResponse.new do |r|
+      r.message body: 'The Robots are coming! Head for the hills!'
+    end
+  
+    content_type 'text/xml'
+  
+    twiml.to_s
+  end
